711730f68d0b2feabc098f970c280f34cccef232
Yvysanna <yvette.violetta@web.de>
2023-10-09 16:52:17 +0200

    ontology

--- app/app.py
+++ app/app.py
@@ -18,9 +18,19 @@ def load_data():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html" )
 
 @app.route("/<beer>")
 def info_page(beer):
 
     return f"{beer}"
+
+
+# TODO:
+# render views.html, return 10 beers
+
+# Onclicking Filter: render something to update views, pass the values of similar beers with all their info
+
+# On clicking of bubble, show pop up image, disable filter option as long as pop up open
+
+# TODO: On clicking 
